95ad1c6ed5854bb5a5a27cf6ff70af97d14e34a5
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-01-17 00:38:42 -0500

    Add Header component to App.js

--- waste-lookup/src/App.js
+++ waste-lookup/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
       </div>
     );
   }
