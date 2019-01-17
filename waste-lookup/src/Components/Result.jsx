a33797f627653b7003c328869007b44b46e3a60f
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-01-17 00:39:32 -0500

    Touch Favourites and Result components

---
+++ waste-lookup/src/Components/Result.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Search = () => {
+  return(
+    <div>
+    </div>
+  )
+};
+
+class Result extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+  }
+}
