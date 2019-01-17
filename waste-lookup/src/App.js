4137e9ae5f6623ee39418fc5fb8a801fcfa0d226
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-01-17 03:40:25 -0500

    Add Result component to root component

--- waste-lookup/src/App.js
+++ waste-lookup/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header'
+import Header from './Components/Header';
+import Result from './Components/Result';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Result />
       </div>
     );
   }
