3a619c17a29009a66ad1d4c81d422d6b0d103a64
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-01-17 14:33:48 -0500

    Complete result styling according to design
    Responsive styling implemented for smaller devices

--- waste-lookup/src/Components/Result.jsx
+++ waste-lookup/src/Components/Result.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './result.css';
 import Star from './Star';
 import PropTypes from 'prop-types';
@@ -9,49 +9,67 @@ class Result extends React.Component {
     super(props);
     this.search = this.search.bind(this);
     this.updateSearch= this.updateSearch.bind(this);
+
     this.items = [];
+    this.handleStarClick = this.handleStarClick.bind(this);
     this.rawData = [];
     this.state = {
       query: '',
-      searchClicked: false,
       displayResults: false,
+      favourites: new Set()
     }
   }
 
   search(e) {
     e.preventDefault();
-    this.setState({searchClicked: true});
-    console.log(this.state.query);
+    this.items = [];
     axios.get('https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000')
       .then(res => {
         this.rawData = res.data;
         for (let i = 0; i < res.data.length; ++i) {
-          if (res.data[i].keywords.includes(this.state.query)) {
+          if (res.data[i].keywords.includes(this.state.query.toLowerCase())) {
             this.items.push(res.data[i]);
           }
         }
-        console.log(this.items);
         this.setState({ displayResults: true });
       });
   }
 
   updateSearch(event) {
     this.setState({query: event.target.value });
-    if (this.state.query === '') {
-      this.setState({displayResults: true});
+    if (this.state.query.length <= 1) {
+      this.setState({displayResults: false});
     }
   }
 
-  handleStarClick(e) {
-    // console.log(e);
-    // this.setState({ isClicked: !this.state.isClicked});
+  handleStarClick(index) {
+    let favourites = this.state.favourites;
+    favourites.add(index);
+    this.setState({ favourites: favourites });
+  }
+
+  escapeHtml(text) {
+      var map = {
+        '&amp;': '&',
+        '&#038;': "&",
+        '&lt;': '<',
+        '&gt;': '>',
+        '&quot;': '"',
+        '&#039;': "'",
+        '&#8217;': "’",
+        '&#8216;': "‘",
+        '&#8211;': "–",
+        '&#8212;': "—",
+        '&#8230;': "…",
+        '&#8221;': '”'
+      };
+      return text.replace(/\&[\w\d\#]{2,5}\;/g, function(m) { return map[m]; });
   }
 
   render() {
     return (
       <section>
         <div className="search-container">
-          {/*Search bar*/}
           <form>
             <div id={'search-container'}>
               <input
@@ -59,7 +77,7 @@ class Result extends React.Component {
                 type="text"
                 onChange={this.updateSearch}
                 value={this.state.query}
-                placeholder="Search for garbage..." />
+                placeholder="Enter garbage item ..." />
               <button onClick={this.search} id={'search-btn'}>
                 <img src={require('../assets/search.png')} alt="Search" id={'main-search-icon'} />
               </button>
@@ -72,8 +90,13 @@ class Result extends React.Component {
               { this.state.displayResults &&
               this.items.map(item => (
                 <tr key={this.rawData.indexOf(item)}>
-                  <td><Star handleStarClick={this.handleStarClick}/></td>
-                  <td>{item.title}</td>
+                  <td className={'star-col'}><Star
+                    handleStarClick={this.handleStarClick}
+                    id={this.rawData.indexOf(item)}
+                    isClicked={this.state.favourites.has(this.rawData.indexOf(item))}
+                  /></td>
+                  <td className={'title-col'}>{item.title}</td>
+                  <td dangerouslySetInnerHTML={{__html: this.escapeHtml(item.body)}}></td>
                 </tr>
               ))}
               </tbody>
@@ -86,9 +109,9 @@ class Result extends React.Component {
 }
 
 Result.propTypes = {
-  // search: PropTypes.func.isRequired,
-  // handleStarClick: PropTypes.func.isRequired,
-  // handleChange: PropTypes.func.isRequired,
+  search: PropTypes.func,
+  handleStarClick: PropTypes.func,
+  updateSearch: PropTypes.func
 };
 
 export default Result;
